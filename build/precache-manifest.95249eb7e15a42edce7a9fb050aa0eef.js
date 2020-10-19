2e606c0159fe3e10b8b3763f9198a402c42b19dd
Gobinath24 <codeitsmart2win@gmail.com>
2020-10-19 12:41:59 +0530

    adding the file to git

---
+++ build/precache-manifest.95249eb7e15a42edce7a9fb050aa0eef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5a4f2eeef88d361447070cc610ab92",
+    "url": "/Gobinath24/RoboDetector/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/Gobinath24/RoboDetector/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5cab08b4835fa9414d50",
+    "url": "/Gobinath24/RoboDetector/static/css/main.ccbc37e7.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/Gobinath24/RoboDetector/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gobinath24/RoboDetector/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cab08b4835fa9414d50",
+    "url": "/Gobinath24/RoboDetector/static/js/main.b2184341.chunk.js"
+  },
+  {
+    "revision": "baee4df9e42e3f03f28a",
+    "url": "/Gobinath24/RoboDetector/static/js/runtime-main.aaa16183.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Gobinath24/RoboDetector/static/media/SEGA.12a3db8b.woff"
+  }
+]);
